d11cc64888816a11c42780aa7d81b30ad30f2bf0
Greg Schloemer <gregs2011@gmail.com>
2020-02-11 11:08:45 -0500

    Initial Commit

---
+++ spark_LastestUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  spark_LastestUI
+//
+//  Created by Greg Schloemer on 2/11/20.
+//  Copyright © 2020 Greg Schloemer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
